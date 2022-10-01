ef1dba542ea05c06da53987aa4ebbe0e701d55d4
Malay-24 <malaybajpai24@gmail.com>
2022-10-01 20:56:28 +0530

    day_5

---
+++ day3/day5/javascript.js
@@ -0,0 +1,101 @@
+document.querySelector("#btn1").addEventListener("click",Btn1);
+document.querySelector("#btn2").addEventListener("click",Btn2);
+document.querySelector("#btn3").addEventListener("click",Btn3);
+document.querySelector("#btn4").addEventListener("click",Btn4);
+
+
+
+function Btn1(){
+ window.open("https://coschedule.com/marketing-calendar","_self");
+}
+
+function Btn2(){
+    window.open("https://coschedule.com/marketing-suite","_self");
+}
+
+function Btn3(){
+    window.open("https://coschedule.com/headline-studio","_self");
+}
+
+function Btn4(){
+    window.open("https://coschedule.com/actionable-marketing-institute/ami-pro","_self");
+}
+
+//  ==========================================================================================================================
+document.querySelector("#box1").addEventListener("click",Box1);
+document.querySelector("#box2").addEventListener("click",Box2);
+document.querySelector("#box3").addEventListener("click",Box3);
+document.querySelector("#Pricing").addEventListener("click",Pricing);
+document.querySelector("#nav-btn").addEventListener("click",NavBtn);
+document.querySelector("#Demo-Btn").addEventListener("click",DemoBtn);
+
+
+
+
+
+
+
+function Box1(){
+    window.open("https://coschedule.com/press/coschedule-featured-on-2022-gartner-magic-quadrant-for-content-marketing-platforms","_self");
+   }
+
+   function Box2(){
+    window.open("https://coschedule.com/press/coschedule-remains-on-inc-5000-list-for-third-consecutive-year","_self");
+   }
+
+   function Box3(){
+    window.open("https://coschedule.com/press/coschedule-marketing-calendar-suite-named-high-performer-for-17th-consecutive-quarter-by-g2-crowd","_self");
+   }
+
+   function Pricing(){
+    window.open("https://coschedule.com/pricing","_self");
+   }
+   function NavBtn(){
+    window.open("https://coschedule.com/product-demo","_self");
+   }
+   function DemoBtn(){
+    window.open("https://coschedule.com/product-demo","_self");
+   }
+
+//    =============================================================================================
+
+document.querySelector("#navImg").addEventListener("click",NavImg);
+
+function NavImg(){
+    location.reload();
+   }
+
+   document.querySelector("#Products").addEventListener("mouseover",Products); 
+
+   function Products(){
+
+   }
+
+//    ================================================================================================================
+
+ document.querySelector("#foot-1").addEventListener("click",Foot1);
+ document.querySelector("#foot-2").addEventListener("click",Foot2);
+ document.querySelector("#foot-3").addEventListener("click",Foot3);
+ document.querySelector("#foot-4").addEventListener("click",Foot4);
+ document.querySelector("#foot-5").addEventListener("click",Foot5);
+ document.querySelector("#foot-6").addEventListener("click",Foot6);
+
+ function Foot1(){
+    window.open("https://www.g2.com/products/coschedule-marketing-suite/reviews");
+ }
+ function Foot2(){
+    window.open("https://www.facebook.com/coschedule");
+ }
+ function Foot3(){
+    window.open("https://twitter.com/coschedule");
+ }
+ function Foot4(){
+    window.open("https://www.linkedin.com/company/coschedule");
+ }
+ function Foot5(){
+    window.open("https://www.pinterest.com/coschedule/");
+ }
+ function Foot6(){
+    window.open("https://www.instagram.com/coschedule/");
+ }
+//  ===============================================================================================================================
